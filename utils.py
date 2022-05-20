4646ea0728e73de754158153469fec41c665874c
XuanShine <xuan.ng@hotmail.com>
2022-05-20 02:59:57 +0200

    selectCalendar & clean

--- utils.py
+++ utils.py
@@ -2,7 +2,9 @@ from pywebio.output import *
 from pywebio.input import *
 from pywebio.pin import *
 from pywebio.session import local
+
 from functools import partial
+from datetime import datetime
 
 def sure(ifYes, ifNo, text="Êtes-vous sûr ?", textOui="Oui", textNon="Non"):
     def wrapper(f):
@@ -14,6 +16,8 @@ def sure(ifYes, ifNo, text="Êtes-vous sûr ?", textOui="Oui", textNon="Non"):
             put_button(textNon, onclick=partial(wrapper, ifNo))
         ])
 
-def clearFamille():
-    local.famille = None
-    clear("famille")
+
+def selectCalendar(func, funcReturn, famille):
+    date = input("Date :", type="date")
+    func(datetime.strptime(date, "%Y-%m-%d").date())
+    return funcReturn(famille.id)
