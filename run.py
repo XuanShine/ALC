1ebb39af2001830b6366fe3ba9b55b46332a5d01
XuanShine <xuan.ng@hotmail.com>
2022-05-25 17:29:45 +0200

    flask integration

--- run.py
+++ run.py
@@ -4,13 +4,18 @@ from pywebio.output import *
 from pywebio_battery import put_logbox
 from pywebio.session import local
 from pywebio.session import set_env
+from pywebio.platform.flask import webio_view
+import pywebio
 
+from flask import Flask
 from functools import partial
 
 from famille import GestionFamille
 import hebergement as hotel
 from create_tables import init_fake_datas
 from account import checkConnection
+
+app = Flask(__name__)
 
 class App:
     def __init__(self):
@@ -20,7 +25,7 @@ class App:
         set_env(output_max_width="80%")
         # put_buttons(["Se Connecter", "S’inscrire"], onclick=self.login)
         self.menu()
-        return
+
     
     def login(self, *args):
         id_ = input("ID:")
@@ -29,7 +34,7 @@ class App:
         self.id = id_
         put_text(f"ID: {self.id}")
         self.menu()
-        return
+
     
     def menu(self):
         put_buttons(buttons=[
@@ -41,11 +46,19 @@ class App:
         ])
         put_scope("famille")
         put_scope("main")
-        return
+
 
 def main():
     App().start()
     
 init_fake_datas()
 
-start_server(main, port=8080, debug=True)
+
+# start_server(main, port=8080, debug=True)
+
+# app.add_url_rule("/app", "webio_view", webio_view(App().start), methods=["GET", "POST", "OPTIONS"])
+
+
+if __name__ == "__main__":
+    # app.run()
+    pywebio.platform.flask.start_server(App().start, port=8080, debug=True)
