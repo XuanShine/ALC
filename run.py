95c3661bd9eea9f9feeca0fb51c88aab09d8b266
XuanShine <xuan.ng@hotmail.com>
2022-05-26 09:27:57 +0200

    port 5001

--- run.py
+++ run.py
@@ -54,7 +54,7 @@ def main():
 init_fake_datas()
 
 
-start_server(main, port=8080, debug=True, host="0.0.0.0")
+start_server(main, port=5001, debug=True, host="0.0.0.0")
 
 # app.add_url_rule("/", "webio_view", webio_view(App().start, cdn="https://cdn.jsdelivr.net/gh/wang0618/PyWebIO-assets@v1.6.1/"), methods=["GET", "POST", "OPTIONS"])
 
